d321230171448528b16f43dbc08af1233f18b440
Jon Stutters <jstutters@jeremah.co.uk>
2017-03-06 23:04:41 +0000

    Start actually drawing some text

--- colournaming/static/namer.js
+++ colournaming/static/namer.js
@@ -1,6 +1,12 @@
 console.log(url)
+var displayCentre;
+var whiteTags;
 
 function setup() {
+    createCanvas(displayWidth, displayHeight);
+    textAlign(CENTER);
+    displayCentre = createVector(displayWidth / 2, displayHeight / 2);
+    whiteTags = false;
     loadJSON(url, drawResponse);
 }
 
@@ -8,7 +14,24 @@ function draw() {
 }
 
 function drawResponse(colour_matches) {
-    c = colour_matches[0];
-    fill(c.red, c.green, c.blue);
-    ellipse(50, 50, 80, 80);
+    background(128);
+    console.log(colour_matches);
+    for (c of colour_matches) {
+        var pos = createVector(c.a, c.b);
+        pos.x = -pos.x; 
+        pos.y = -pos.y;
+        pos.add(displayCentre);
+        if (c.d == 0.0) {
+            if (whiteTags == false) {
+                fill(c.red, c.green, c.blue, 255);
+            }
+            textSize(32);
+        } else {
+            if (whiteTags == false) {
+                fill(c.red, c.green, c.blue, 200);
+            }
+            textSize(20);
+        }
+        text(c.name, pos.x, pos.y);
+    }
 }
