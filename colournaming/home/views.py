f81de7b079707e25bb1e1689ad2382525c4e4dd8
Jon Stutters <jstutters@jeremah.co.uk>
2017-05-01 11:39:17 +0100

    Merge changes from origin

--- colournaming/home/views.py
+++ colournaming/home/views.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 """Views for the front page."""
 
 from flask import Blueprint, render_template
@@ -24,3 +25,22 @@ def contact():
     if form.validate_on_submit():
         pass
     return render_template('contact.html', form=form)
+=======
+"""Views for the front page."""
+
+from flask import Blueprint, render_template
+
+bp = Blueprint('home', __name__)
+
+
+@bp.route('/')
+def index():
+    """Render the front page."""
+    return render_template('index.html')
+
+
+@bp.route('terms_and_conditions.html')
+def terms_and_conditions():
+    """Render the terms and conditions page."""
+    return render_template('terms_and_conditions.html')
+>>>>>>> Stashed changes
