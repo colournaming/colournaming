63e04ad33e218ae9c53836bd40fd8717c80d161e
Jon Stutters <jstutters@jeremah.co.uk>
2017-05-16 22:46:39 +0100

    Adding more experiment stages

--- colournaming/experiment/forms.py
+++ colournaming/experiment/forms.py
@@ -1,5 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import SelectField
+from wtforms import DecimalField, SelectField
 
 class GreyscaleLevelsForm(FlaskForm):
     levels = SelectField(choices=[(x, x) for x in range(13)], coerce=int)
+
+
+class ObserverInformation(FlaskForm):
+    age = DecimalField(description="What is your age?")
+    gender = SelectField(choices=[('na', 'No answer'), ('f', 'Female'), ('m', 'Male'), ('t', 'Transgender')])
--- colournaming/experiment/views.py
+++ colournaming/experiment/views.py
@@ -23,3 +23,21 @@ def display_properties():
 @bp.route('/colour_vision.html')
 def colour_vision():
     return render_template('colour_vision.html')
+
+
+@bp.route('/name_colour.html')
+def name_colour():
+    return render_template('name_colour.html')
+
+
+@bp.route('/observer_information.html')
+def observer_information():
+    form = forms.ObserverInformation()
+    if form.validate_on_submit():
+        return redirect(url_for('experiment.thankyou'))
+    return render_template('observer_information.html', form=form)
+
+
+@bp.route('/thankyou.html')
+def thankyou():
+    return render_template('thankyou.html')
