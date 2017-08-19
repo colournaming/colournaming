5d59544b109deb4ba736093ee8ba4ca19a618a94
Jon Stutters <jstutters@jeremah.co.uk>
2017-08-19 14:10:47 +0100

    Improve display and observer information forms

--- colournaming/experiment/forms.py
+++ colournaming/experiment/forms.py
@@ -1,6 +1,7 @@
 """Forms used in the colour naming experiment."""
 
 from flask_wtf import FlaskForm
+import pycountry
 from wtforms import DecimalField, HiddenField, SelectField, TextField
 from wtforms.widgets import HiddenInput
 from . import model
@@ -15,19 +16,46 @@ class DisplayForm(FlaskForm):
     levels = SelectField(choices=[(x, x) for x in range(13)], coerce=int)
     screen_height = HiddenField()
     screen_width = HiddenField()
-    device_orientation = DecimalField(widget=HiddenInput())
-    ambient_light = SelectField(choices=_enum_to_choices(model.AmbientLight))
-    screen_light = SelectField(choices=_enum_to_choices(model.ScreenLight))
+    ambient_light = SelectField(
+        choices=_enum_to_choices(model.AmbientLight),
+        description="What is the level of ambient lighting where you are?"
+    )
+    screen_light = SelectField(
+        choices=_enum_to_choices(model.ScreenLight),
+        description="What is the lighting level behind your screen?"
+    )
 
 
 class ObserverInformationForm(FlaskForm):
     """Form for gathering observer information."""
 
     age = DecimalField(description="What is your age?")
-    gender = SelectField(choices=_enum_to_choices(model.Gender))
-    colour_experience = SelectField(choices=_enum_to_choices(model.ColourExperience))
-    language_experience = SelectField(choices=_enum_to_choices(model.LanguageExperience))
-    education_level = SelectField(choices=_enum_to_choices(model.EducationLevel))
+    gender = SelectField(
+        choices=_enum_to_choices(model.Gender),
+        description="What is your gender?"
+    )
+    colour_experience = SelectField(
+        choices=_enum_to_choices(model.ColourExperience),
+        description="What level of experience do you have working with colour?"
+    )
+    language_experience = SelectField(
+        choices=_enum_to_choices(model.LanguageExperience),
+        description="What experience do you have with your language?"
+    )
+    education_level = SelectField(
+        choices=_enum_to_choices(model.EducationLevel),
+        description="What level of education have you attained?"
+    )
+    user_agent = HiddenField()
+    ip_address = HiddenField()
+    country_raised = SelectField(
+        choices=[(x.alpha_2, x.name) for x in pycountry.countries],
+        description="In which country were you raised?"
+    )
+    country_resident = SelectField(
+        choices=[(x.alpha_2, x.name) for x in pycountry.countries],
+        description="In which country are you currently living?"
+    )
 
 
 class ColourVisionForm(FlaskForm):
