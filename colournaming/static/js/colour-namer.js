5bf2fb7fe31ad9ff402735ed99b5160000d96c80
chrisguttandin <chrisguttandin@media-codings.com>
2017-10-13 16:26:20 +0200

    implement colour picker

--- colournaming/static/js/colour-namer.js
+++ colournaming/static/js/colour-namer.js
@@ -1,3 +1,38 @@
 $(function () {
-    const colourPicker = $.farbtastic('#colour-picker');
+    function hexToRGB (hexcode) {
+        return [
+            parseInt(hexcode.substring(0, 2), 16),
+            parseInt(hexcode.substring(2, 4), 16),
+            parseInt(hexcode.substring(4, 6), 16)
+        ];
+    }
+
+    function doQuery (hexcode, responseHandler) {
+        hexcode = hexcode.substring(1);
+
+        var rgb = hexToRGB(hexcode);
+        var hexString = 'Hex: ' + hexcode.toUpperCase();
+        var rgbString = 'RGB: ' + rgb.join(', ');
+
+        $('#stats-box-rgb-display').text(rgb.join(' '));
+        $('#stats-box-header').css('background-color', hexcode);
+
+        $.getJSON(COLOUR_NAMER_URL + '?colour=' + hexcode, function (data) {
+            var colourName = data.colours.shift().name;
+            var $synonyms = data.colours
+                .map(function (colour) {
+                    return $('<li>').text(colour.name);
+                })
+                .reduce(function ($ul, $li) {
+                    return $ul.append($li);
+                }, $('<ul>'));
+
+            $('#stats-box-colour-name-display').text(colourName);
+            $('#stats-box-synonyms-display ul').replaceWith($synonyms);
+        });
+    }
+
+    $.farbtastic('#colour-picker', function (colour) {
+        doQuery(colour);
+    }).setColor('#FF0000');
 })
