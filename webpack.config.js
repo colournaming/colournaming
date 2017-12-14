07877a554a94249a84f987ff8b5398cbd70e22a2
Jon Stutters <j.stutters@ucl.ac.uk>
2017-12-14 11:42:29 +0000

    Add babel and uglifyjs

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const webpack = require('webpack')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = {
   entry: './assets/js/colour-namer.js',
@@ -7,7 +8,17 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'colournaming/static')
   },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader'
+    }]
+  },
   plugins: [
+    new UglifyJsPlugin({
+      test: /\.js$/,
+    }),
     new webpack.ProvidePlugin({
       $: 'jquery',
       jQuery: 'jquery'
