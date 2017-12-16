681be761c99470cafe17601fd4f55aa0d7ee6637
Jon Stutters <jstutters@jeremah.co.uk>
2017-12-16 12:04:03 +0000

    Cleanup javascript files

--- colournaming/static/initnamer-mobile.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-    $('#goButton').on('click', doQuery);
-    $('#colourpicker').farbtastic(function(e) { doQuery(e, updateWithResponse) }); 
-    var $farb = $('.farbtastic')[0];
-    $farb.style['margin-left'] = 'auto';
-    $farb.style['margin-right'] = 'auto';
-})
--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,12 @@ const path = require('path')
 const webpack = require('webpack')
 
 module.exports = {
-  entry: './assets/js/colour-namer.js',
+  entry: {
+    namer: './assets/js/colour-namer.js',
+    experiment: './assets/js/experiment.js'
+  },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].js',
     path: path.resolve(__dirname, 'colournaming/static')
   },
   plugins: [
