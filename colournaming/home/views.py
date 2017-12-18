7d38380a5c69e0b5956eb0c947f4a009d010021d
Jon Stutters <j.stutters@ucl.ac.uk>
2017-12-18 14:56:58 +0000

    Remove contact endpoint

--- colournaming/home/views.py
+++ colournaming/home/views.py
@@ -45,22 +45,3 @@ def research():
 def terms_and_conditions():
     """Render the terms and conditions page."""
     return render_template('terms_and_conditions.html')
-
-
-@bp.route('contact.html')
-def contact():
-    """Render the contact page."""
-    form = ContactForm()
-    if form.validate_on_submit():
-        msg = Message(
-            'ColourNamer message from {0} {1}'.format(form.first_name, form.last_name),
-            sender='contact@colornaming.net',
-            recipients=[current_app.config['CONTACT_EMAIL']]
-        )
-        msg.body = (
-            'FROM: {0} {1} <{2}>\n'
-            'ORGANISATION: {3}\n\n'
-            '{4}'
-        )
-        mail.send(msg)
-    return render_template('contact.html', form=form)
