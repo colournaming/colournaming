3553fdbf41694977285dbda769bcf0993373af0c
Jon Stutters <j.stutters@ucl.ac.uk>
2018-02-13 12:56:22 +0000

    Remove extra semicolon

--- colournaming/static/namer.js
+++ colournaming/static/namer.js
@@ -10462,7 +10462,7 @@ $(function () {
     doQuery(hexCode);
   }).setColor('#FF0000');
 
-  $(document).on('touch', _audio.resumeAudio);;
+  $(document).on('touch', _audio.resumeAudio);
   $languageSelect.change(onLanguageSelectChange);
   $nameSelect.change(onColourSelectChange);
   $agreementSelect.change(onAgreementSelectChange);
