b5d7288d1cc567fa3d82fcf08f9044fe8ebea174
Jon Stutters <jstutters@jeremah.co.uk>
2024-08-09 15:59:40 +0200

    Use colbg targets in mturkage

--- colournaming/mturkage/controller.py
+++ colournaming/mturkage/controller.py
@@ -8,7 +8,7 @@ from sqlalchemy.sql.expression import func
 from colournaming.mturk.exceptions import MTurkIDNotFound
 from ..database import db
 from .model import MturkAgeTask, MturkAgeParticipant, MturkAgeColourResponse
-from ..experimentcol.model import ColourTarget
+from ..experimentcolbg.model import ColourTargetColBG
 
 
 def read_targets_from_file(targets_file, delete_existing=False):
